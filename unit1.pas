67910437a3dd98317593c8e5fc188acb2b30285e
jdelauney <delauney.jerome@gmail.com>
2020-08-12 12:49:29 +0200

    Update and improve performance
    Update and improve performance

--- unit1.pas
+++ unit1.pas
@@ -5,65 +5,70 @@ unit Unit1;
 
 interface
 
+{
+  CODE1 = Bitmap.Canvas
+  CODE2 = Panel.Canvas
+  CODE3 = Windows native
+  CODE4 = BGRABitmap (https://github.com/bgrabitmap/bgrabitmap)
+  CODE5 = IntfImage
+  CODE6 = BZScene (https://github.com/jdelauney/BZScene)
+
+--------------------------------------------------------------------------------
+
+  Average frame rate in ms, benchmark template
+ ______________________________________________________________________________
+ |  CPU     :                                                                 |
+ |  OS      :                                                                 |
+ |  FPC     :                                                                 |
+ |  LAZARUS :                                                                 |
+ |____________________________________________________________________________|
+ |  MEHTOD |  CODE 1  |  CODE 2  |  CODE 3  |  CODE 4  |  CODE 5  |  CODE 6   |
+ |=========|==========|==========|==========|==========|==========|===========|
+ |  Debug  |          |          |          |          |          |           |
+ |---------|----------|----------|----------|----------|----------|-----------|
+ | Release |          |          |          |          |          |           |
+ |----------------------------------------------------------------------------|
+
+--------------------------------------------------------------------------------
+                                 BeanzMaster
+ ______________________________________________________________________________
+ |  CPU     : AMD A10-7870K Radeon R7, 12 Compute Cores 4C+8G                 |
+ |  OS      : Windows 10 64bit                                                |
+ |  FPC     : 3.2.0                                                           |
+ |  LAZARUS : 2.0.10                                                          |
+ |____________________________________________________________________________|
+ |  MEHTOD |  CODE 1  |  CODE 2  |  CODE 3  |  CODE 4  |  CODE 5  |  CODE 6   |
+ |=========|==========|==========|==========|==========|==========|===========|
+ |  Debug  | 39,9572  | 686,1903 |  0,5434  |  1,2037  |  1,5852  |  0,8171   |
+ |---------|----------|----------|----------|----------|----------|-----------|
+ | Release | 38,7973  | 671,5684 |  0,4201  |  1,0717  |  1,3234  |  0,7014   |
+ |----------------------------------------------------------------------------|
+
+ ______________________________________________________________________________
+ |  CPU     : AMD A10-7870K Radeon R7, 12 Compute Cores 4C+8G                 |
+ |  OS      : Linux Manjaro 64bit                                             |
+ |  FPC     : 3.2.0                                                           |
+ |  LAZARUS : 2.0.10                                                          |
+ |____________________________________________________________________________|
+ |  MEHTOD |  CODE 1  |  CODE 2  |  CODE 3  |  CODE 4  |  CODE 5  |  CODE 6   |
+ |=========|==========|==========|==========|==========|==========|===========|
+ |  Debug  | 11,2772  |  35,1391 |   N/A    |  2,3768  |  3,1898  |  2,1478   |
+ |---------|----------|----------|----------|----------|----------|-----------|
+ | Release | 11,2917  |  37,6608 |   N/A    |  2,1562  |  2,7684  |  2,0740   |
+ |----------------------------------------------------------------------------|
+
+--------------------------------------------------------------------------------
+}
+
 {$DEFINE CODE1}
 
 uses
   {$IFDEF WINDOWS}Windows, {$ENDIF}Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls,
   LCLType, LCLProc
   {$IFDEF CODE4}, BGRABitmap, BGRABitmapTypes{$ENDIF}
-  {$IFDEF CODE5}, IntfGraphics, GraphType{$ENDIF};
+  {$IFDEF CODE5}, IntfGraphics, GraphType{$ENDIF}
+  {$IFDEF CODE6}, BZColors, BZGraphic, BZBitmap{$ENDIF};
 
-type
-  TLargeArray=array[0..65535] of byte;
-
-type
-  { TForm1 }
-
-  TForm1 = class(TForm)
-    Panel1: TPanel;
-    procedure FormCreate(Sender: TObject);
-		procedure FormDestroy(Sender: TObject);
-    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-		procedure Panel1Paint(Sender: TObject);
-  private
-    MP, Scr: TLargeArray;
-    Dir, PosX, PosY: integer;
-    Freq:Int64;
-    {$IF defined(CODE1) or defined(CODE3) or defined(CODE5)}
-    bmp: TBitmap;
-    {$ENDIF}
-    {$IFDEF CODE3}
-    Header:Windows.PBitmapInfo;
-    {$ENDIF}
-    {$IFDEF CODE4}
-    bgra:TBGRABitmap;
-    {$ENDIF}
-    {$IFDEF CODE5}
-    IntfImage:TLazIntfImage;
-    {$ENDIF}
-    {$IF defined(CODE1) or defined(CODE2)}
-    procedure UpdateBitmap(C: TCanvas);
-    {$ENDIF}
-    {$IFDEF CODE3}
-    procedure MyUpdateBitmap(C: TCanvas);
-    {$ENDIF}
-    {$IFDEF CODE4}
-    procedure MyUpdateBitmap2(C: TCanvas);
-    {$ENDIF}
-    {$IFDEF CODE5}
-    procedure MyUpdateBitmap3(C: TCanvas);
-    {$ENDIF}
-    procedure UpdateImage;
-  public
-
-  end;
-
-var
-  Form1: TForm1;
-
-implementation
-
-{$R *.lfm}
 
 const
  SizeX=320;
@@ -87,18 +92,99 @@ const
   8,57,40,8,56,39,7,56,38,6,55,37,5,55,35,4,54,33,4,54,31,2,32,32,32,63,63,63,
   63,63,63,63,63,63,63,63,63,48,48,48,63,63,63,63,63,63);
 
-{$IF defined(CODE1) or defined(CODE2)}
+type
+  TLargeArray=array[0..65535] of byte;
+
+type
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+		procedure FormDestroy(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+		procedure Panel1Paint(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormKeyPress(Sender: TObject; var Key: char);
+  private
+    MP, Scr: TLargeArray;
+    Dir, PosX, PosY: integer;
+
+
+    FrameCounter, Freq : Int64;
+    TotalTime : Double;
+
+    voxdep : array[1..689] of char;
+    cDep : Integer;
+    SinMoveLut, CosMoveLUT : Array[-360..360] of integer;
+    SinLut256, CosLut256 : Array[-360..360] of integer;
+
+    {$IF defined(CODE1) or defined(CODE2)}
+      palColor: array[1..length(pal) div 3] of TColor;
+    {$ENDIF}
+
+    {$IF defined(CODE1) or defined(CODE3) or defined(CODE5)}
+    bmp: TBitmap;
+    {$ENDIF}
+
+    {$IFDEF CODE3}
+    Header:Windows.PBitmapInfo;
+    {$ENDIF}
+
+    {$IFDEF CODE4}
+    bgra:TBGRABitmap;
+    palBGRA: array[1..length(pal) div 3] of TBGRAPixel;
+    {$ENDIF}
+
+    {$IFDEF CODE5}
+    IntfImage:TLazIntfImage;
+    palIntf: array[1..length(pal) div 3] of Longword;
+    {$ENDIF}
+
+    {$IFDEF CODE6}
+    bzBmp:TBZBitmap;
+    palBZScene: array[1..length(pal) div 3] of TBZColor;
+    {$ENDIF}
+
+
+    procedure DrawVoxel(xp, yp, aDir:integer; var map: TLargeArray);
+
+    {$IF defined(CODE1) or defined(CODE2)}
+    procedure UpdateBitmap;
+    {$ENDIF}
+
+    {$IFDEF CODE3}
+    procedure MyUpdateBitmap(C: TCanvas);
+    {$ENDIF}
+
+    {$IFDEF CODE4}
+    procedure MyUpdateBitmap2(C: TCanvas);
+    {$ENDIF}
+
+    {$IFDEF CODE5}
+    procedure MyUpdateBitmap3(C: TCanvas);
+    {$ENDIF}
+
+    {$IFDEF CODE6}
+    procedure MyUpdateBitmap4(C: TCanvas);
+    {$ENDIF}
+
+    procedure UpdateImage;
+  public
+
+  end;
+
 var
-  palColor: array[1..length(pal) div 3] of TColor;
-{$ENDIF}
-{$IFDEF CODE4}
-var
-  palBGRA: array[1..length(pal) div 3] of TBGRAPixel;
-{$ENDIF}
-{$IFDEF CODE5}
-var
-  palIntf: array[1..length(pal) div 3] of Longword;
-{$ENDIF}
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+
+
+
 
 function GetFreq:Int64;
 begin
@@ -147,39 +233,90 @@ begin
   plasma(x1,yn,xn,y2,mp); plasma(xn,yn,x2,y2,mp);
 end;
 
-procedure draw(xp,yp,dir:integer; var scr,mp: TLargeArray);
+procedure TForm1.DrawVoxel(xp, yp, aDir:integer; var map: TLargeArray);
 var
-  z,zobs,ix,iy,iy1,iyp,ixp,x,y,s,csf,snf,mpc,i,j:integer;
+  z,zobs,ix,iy,iy1,iyp,ixp,x,y,s,csf,snf,mpc,i,j, dpiy, dpix :integer;
   rng: array[0..320] of byte;
 begin
-  fillchar(rng,sizeof(rng),200); zobs:=100+mp[WORD(256*yp+xp)];
-  csf:=round(256*cos((dir)/180*pi)); snf:=round(256*sin((dir)/180*pi));
+  fillchar(rng,sizeof(rng),200);
+  zobs := 100 + map[WORD(yp shl 8 + xp)];
+  csf := CosLUT256[aDir];
+  snf := SinLUT256[aDir];
+
+  {$IFDEF CODE1}
+  With bmp.Canvas do
+  begin
+    Brush.Style := bsSolid;
+    Brush.Color := clBlack;
+    Rectangle(0,0,SizeX - 1, SizeY -1);
+  end;
+  {$ENDIF}
+
+  {$IF defined(CODE2) or defined(CODE3) or defined(CODE5)}
   fillchar(scr,SizeOf(scr),0);
+  {$ENDIF}
+
+  {$IFDEF CODE4}
+  bgra.Fill(BGRABlack);
+  {$ENDIF}
+
+  {$IFDEF CODE6}
+  bzBmp.Clear(clrBlack);
+  {$ENDIF}
+
   for iy:=yp to yp+55 do
    begin
-    iy1:=1+2*(iy-yp); s:=4+300 div iy1;
-    for ix:=xp+yp-iy to xp-yp+iy do
-     begin
-      ixp:=xp+((ix-xp)*csf+(iy-yp)*snf) shr 8;
-      iyp:=yp+((iy-yp)*csf-(ix-xp)*snf) shr 8;
-      x:=160+360*(ix-xp) div iy1;
+    dpiy := (iy-yp);
+    iy1 := 1 + (dpiy + dpiy);
+    s := 4 + 300 div iy1;
+    for ix := (xp + yp - iy) to (xp - yp + iy) do
+    begin
+      dpix := (ix-xp);
+      ixp := xp + (dpix * csf + dpiy * snf) shr 8;
+      iyp := yp + (dpiy * csf - dpix * snf) shr 8;
+      x := 160 + 360 * dpix div iy1;
       if (x>=0) and (x+s<=318) then
        begin
-        z:=mp[WORD(iyp shl 8+ixp)]; mpc:=z shr 1;
-        if z<47 then z:=46;  y:=100+(zobs-z)*30 div iy1;
+        z := map[WORD(iyp shl 8 + ixp)];
+        mpc:=z shr 1;
+        if (z<47) then z:=46;
+        y := 100 + (zobs-z)*30 div iy1;
         if (y<=199) and (y>=0) then
-         for j:=x to x+s do
+        begin
+          for i := x to x+s do
           begin
-           for i:=y to rng[j] do
-            scr[WORD(320*i+j)]:=mpc;
-           if y<rng[WORD(j)] then rng[WORD(j)]:=y
+            for j:=y to rng[i] do
+            begin
+              {$IFDEF CODE1}
+                bmp.Canvas.Pixels[i, j] := palColor[mpc];
+              {$ENDIF}
+
+              {$IF Defined(CODE2) or Defined(CODE3) or Defined(CODE5)}
+                scr[WORD(320 * j + i)] := mpc; // calcul of offset can be improve with two SHL
+              {$ENDIF}
+
+              {$IFDEF CODE4}
+                bgra.DrawPixel(i,j, palBgra[mpc]);
+              {$ENDIF}
+
+              {$IFDEF CODE6}
+                bzBmp.SetPixel(i, j, palBZScene[mpc]);
+              {$ENDIF}
+            end;
+            if (y < rng[WORD(i)]) then rng[WORD(i)] := y;
           end;
+        end;
       end;
     end;
   end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
+Var
+  I:Integer;
+  {$IFDEF CODE5}
+  lRawImage: TRawImage;
+  {$ENDIF}
 
   {$IF defined(CODE1) or defined(CODE2)}
   procedure ComputePalColor;
@@ -227,6 +364,7 @@ procedure TForm1.FormCreate(Sender: TObject);
       ByteR:= (pal[IndexPal+1] shl 2) + (pal[IndexPal+1] shr 4);
       ByteG:= (pal[IndexPal+2] shl 2) + (pal[IndexPal+2] shr 4);
       ByteB:= (pal[IndexPal+3] shl 2) + (pal[IndexPal+3] shr 4);
+
       if IntfImage.DataDescription.ByteOrder = riboLSBFirst then
         palIntf[NColor]:= NtoLE(LongWord((ByteR shl IntfImage.DataDescription.RedShift) +
                           (ByteG shl IntfImage.DataDescription.GreenShift) +
@@ -237,6 +375,23 @@ procedure TForm1.FormCreate(Sender: TObject);
                           (ByteG shl IntfImage.DataDescription.GreenShift) +
                           (ByteB shl IntfImage.DataDescription.BlueShift) +
                           (255 shl IntfImage.DataDescription.AlphaShift)));
+    end;
+  end;
+  {$ENDIF}
+
+  {$IFDEF CODE6}
+  procedure ComputePalBZScene;
+  var
+    IndexPal, NColor: Integer;
+    ByteB, ByteG, ByteR: Byte;
+  begin
+    for NColor := 1 to length(palBZScene) do
+    begin
+      IndexPal:= NColor*3;
+      ByteR:= (pal[IndexPal+1] shl 2) + (pal[IndexPal+1] shr 4);
+      ByteG:= (pal[IndexPal+2] shl 2) + (pal[IndexPal+2] shr 4);
+      ByteB:= (pal[IndexPal+3] shl 2) + (pal[IndexPal+3] shr 4);
+      palBZScene[NColor]:= BZColor(ByteR, ByteG, ByteB);
     end;
   end;
   {$ENDIF}
@@ -272,10 +427,37 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
   Freq := GetFreq;
 
-  Randomize;
+  for i:=-360 to 360 do
+  begin
+    CosMoveLUT[I] := round(5 * System.Sin((Dir)/180*pi));
+    SinMoveLUT[I] := round(5 * System.Cos((Dir)/180*pi));
+  End;
+
+  for i:=-360 to 360 do
+  begin
+    SinLUT256[I] := round(256 * System.Sin(I / 180 * pi));
+    CosLUT256[I] := round(256 * System.Cos(I / 180 * pi));
+  End;
+
+  //Randomize;
+  RandSeed := 1234567; // To generate always the same landscape for comparing each method
+
+  // Construct a travel way
+  for i:=1 to 100 do voxDep[i]:=#72;
+  for i:=101 to 130 do voxDep[i]:=#76;
+  for i:=131 to 231 do voxDep[i]:=#72;
+  for i:=232 to 267 do voxDep[i]:=#78;
+  for i:=268 to 367 do voxDep[i]:=#75;
+  for i:=368 to 450 do voxDep[i]:=#72;
+  for i:=451 to 530 do voxDep[i]:=#77;
+  for i:=531 to 688 do voxDep[i]:=#80;
+  voxDep[689]:=#32; // Stop
+  cDep:=0;
+
   PosX:=0;
   PosY:=0;
   Dir:=0;
+
   FillChar(MP,SizeOf(MP),0);
   MP[$0000]:=128;
   plasma(0,0,256,256,MP);
@@ -283,6 +465,7 @@ begin
   {$IF defined(CODE1) or defined(CODE3) or defined(CODE5)}
   bmp:= TBitmap.Create;
   {$ENDIF}
+  
   {$IF defined(CODE1) or defined(CODE3)}
   bmp.SetSize(SizeX, SizeY);
   {$ENDIF}
@@ -303,11 +486,29 @@ begin
   {$IFDEF CODE5}
   IntfImage := TLazIntfImage.Create(SizeX, SizeY, [riqfRGB, riqfAlpha]);
   IntfImage.CreateData;
+  //lRawImage.Init;
+  //  {$IFDEF WINDOWS}
+  //    lRawImage.Description.Init_BPP32_B8G8R8A8_BIO_TTB(SizeX, SizeY);
+  //  {$ELSE}
+  //    lRawImage.Description.Init_BPP32_R8G8B8A8_BIO_TTB(SizeX, SizeY);
+  //  {$ENDIF}
+  //lRawImage.CreateData(True);
+  //
+  //IntfImage := TLazIntfImage.Create(0,0);
+  //IntfImage.SetRawImage(lRawImage);
+
   ComputePalIntf;
+  {$ENDIF}
+
+  {$IFDEF CODE6}
+  bzBmp := TBZBitmap.Create(SizeX, SizeY, clrBlack);
+  ComputePalBZScene;
   {$ENDIF}
 
   ClientWidth:= SizeX;
   ClientHeight:= SizeY;
+  TotalTime := 0;
+  FrameCounter := 0;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
@@ -324,29 +525,45 @@ begin
   {$IFDEF CODE5}
   IntfImage.Free;
   {$ENDIF}
+  {$IFDEF CODE6}
+  FreeAndNil(bzBmp);
+  {$ENDIF}
 end;
 
 {$IF defined(CODE1) or defined(CODE2)}
-procedure TForm1.UpdateBitmap(C: TCanvas);
+procedure TForm1.UpdateBitmap;
+{$IFDEF CODE2}
 var
   i, j: integer;
   Src: PByte;
+{$ENDIF}
 begin
-  draw(PosX,PosY,Dir,Scr,MP);
+  {$IFDEF CODE1}
+  bmp.BeginUpdate(true);
+  {$ENDIF}
+  drawVoxel(PosX,PosY,Dir,MP);
+  {$IFDEF CODE1}
+  bmp.EndUpdate();
+  {$ENDIF}
+
+  {$IFDEF CODE2}
   Src := @Scr;
   for j:= 0 to SizeY-1 do
+  begin
     for i:= 0 to SizeX-1 do
     begin
-      C.Pixels[i, j]:= palColor[Src^];
+      Panel1.Canvas.Pixels[i, j]:= palColor[Src^];
       inc(Src);
     end;
+  end;
+  {$ENDIF}
 end;
 {$ENDIF}
 
 {$IFDEF CODE3}
 procedure TForm1.MyUpdateBitmap(C: TCanvas);
 begin
-  draw(PosX,PosY,Dir,Scr,MP);
+  drawVoxel(PosX,PosY,Dir,MP);
   SetDIBitsToDevice(
     C.Handle,
     0,
@@ -366,22 +583,8 @@ end;
 
 {$IFDEF CODE4}
 procedure TForm1.MyUpdateBitmap2(C: TCanvas);
-var
-  i, j: integer;
-  Src:PByte;
-  Dest:PBGRAPixel;
 begin
-  draw(PosX,PosY,Dir,Scr,MP);
-  Src := @Scr;
-  for i:= 0 to SizeY-1 do begin
-    Dest := bgra.ScanLine[i];
-    for j:= 0 to SizeX-1 do begin
-      Dest^ := palBGRA[Src^];
-      Inc(Src);
-      Inc(Dest);
-    end;
-  end;
-  bgra.InvalidateBitmap;
+  drawVoxel(PosX,PosY,Dir,MP);
   bgra.Draw(C, 0, 0);
 end;
 {$IFEND}
@@ -392,36 +595,64 @@ var
   i, j: integer;
   Src:PByte;
   Dest:PLongword;
-  Bitmap, Mask:HBITMAP;
+  //Bitmap, Mask:HBITMAP;
 begin
-  draw(PosX,PosY,Dir,Scr,MP);
+
+  drawVoxel(PosX,PosY,Dir,MP);
+
   IntfImage.BeginUpdate;
   Dest := PLongword(IntfImage.PixelData);
   Src := @Scr;
-  for i:= 0 to SizeY-1 do begin
-    for j:= 0 to SizeX-1 do begin
+  for i:= 0 to SizeY-1 do
+  begin
+    for j:= 0 to SizeX-1 do
+    begin
       Dest^ := palIntf[Src^];
       Inc(Src);
       Inc(Dest);
     end;
   end;
   IntfImage.EndUpdate;
+
+  //IntfImage.CreateBitmaps(Bitmap, Mask, True);
+  //Bmp.Handle := Bitmap;
+  //bmp.MaskHandle := Mask;
+
   bmp.LoadFromIntfImage(IntfImage);
+
+  // Need to clear background canvas
+  With C do
+  begin
+    Brush.Style := bsSolid;
+    Brush.Color := clBlack;
+    Rectangle(0,0,SizeX - 1, SizeY -1);
+  end;
+
   C.Draw(0, 0, bmp);
 end;
 {$IFEND}
 
+{$IFDEF CODE6}
+procedure TForm1.MyUpdateBitmap4(C: TCanvas);
+begin
+  drawVoxel(PosX,PosY,Dir,MP);
+  bzBmp.DrawToCanvas(C, Panel1.ClientRect);
+end;
+{$IFEND}
+
 procedure TForm1.UpdateImage;
-  var Tick:Int64;
+var
+  Tick:Int64;
+  d : Double;
 begin
   Tick := GetCounter;
   {$IFDEF CODE1}
-  UpdateBitmap(bmp.Canvas);
+  UpdateBitmap;
   Panel1.Canvas.Draw(0, 0, bmp);
   {$ENDIF}
 
   {$IFDEF CODE2}
-  UpdateBitmap(Panel1.Canvas);
+  UpdateBitmap;
   {$ENDIF}
 
   {$IFDEF CODE3}
@@ -433,10 +664,18 @@ begin
   {$ENDIF}
 
   {$IFDEF CODE5}
+  //Panel1.Canvas.Clear;
   MyUpdateBitmap3(Panel1.Canvas);
   {$ENDIF}
 
-  Caption := FloatToStr((GetCounter - Tick) * 1000 / Freq) + 'ms';
+  {$IFDEF CODE6}
+  MyUpdateBitmap4(Panel1.Canvas);
+  {$ENDIF}
+
+  d := (GetCounter - Tick) * 1000 / Freq;
+  TotalTime := TotalTime + d;
+  inc(FrameCounter);
+  Caption := FloatToStr(d) + 'ms';
 end;
 
 procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
@@ -461,8 +700,8 @@ begin
 
   if (Key=VK_UP) and (shift=[]) then
   begin
-    PosY:=PosY+round(5*cos((Dir)/180*pi));
-    PosX:=PosX+round(5*sin((Dir)/180*pi));
+    PosY := PosY + CosMoveLUT[Dir];
+    PosX := PosX + SinMoveLUT[Dir];
     key:= 0;
     Panel1.Repaint;
     exit;
@@ -470,8 +709,8 @@ begin
 
   if (Key=VK_DOWN) and (shift=[]) then
   begin
-    PosY:=PosY-round(5*cos((Dir)/180*pi));
-    PosX:=PosX-round(5*sin((Dir)/180*pi));
+    PosY := PosY - CosMoveLUT[Dir];
+    PosX := PosX - SinMoveLUT[Dir];
     key:= 0;
     Panel1.Repaint;
     exit;
@@ -481,6 +720,65 @@ end;
 procedure TForm1.Panel1Paint(Sender: TObject);
 begin
   UpdateImage;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+var
+  Dep : Char;
+Begin
+  Dir := Dir mod 360;
+  {$if not(defined(CODE1)) and not(Defined(CODE6))}
+     UpdateImage;
+  {$ELSE}
+    Panel1.Repaint;
+  {$ENDIF}
+  inc(cdep);
+  Dep := voxDep[cDep];
+  Case Dep of
+    #75:
+    begin
+      Dec(Dir,5);
+    End;
+    #77:
+    begin
+      Inc(Dir,5);
+    End;
+    #76:
+    begin
+      Dec(Dir,5);
+      PosY := PosY + CosMoveLUT[Dir];
+      PosX := PosX + SinMoveLUT[Dir];
+    End;
+    #78 :
+    begin
+      Inc(Dir,5);
+      PosY := PosY + CosMoveLUT[Dir];
+      PosX := PosX + SinMoveLUT[Dir];
+    End;
+    #72 :
+    begin
+      PosY := PosY + CosMoveLUT[Dir];
+      PosX := PosX + SinMoveLUT[Dir];
+    End;
+    #80 :
+    begin
+      PosY := PosY - CosMoveLUT[Dir];
+      PosX := PosX - SinMoveLUT[Dir];
+    End;
+    #32:
+    begin
+      Timer1.Enabled := false;
+      ShowMessage('Average Frame speed : ' + FloatToStr(TotalTime / FrameCounter) + ' ms');
+    End;
+  End;
+end;
+
+procedure TForm1.FormKeyPress(Sender: TObject; var Key: char);
+begin
+  if UpCase(Key) = 'A' then
+  begin
+    Timer1.Enabled := True;
+  end;
 end;
 
 end.
